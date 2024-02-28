7c7edacef2390aec1403d2a5a1658b47b8823647
Aidan <aidan.christie10@gmail.com>
2024-02-27 22:29:23 -0500

    Initial commit

---
+++ src/main/java/net/adwr/breathewater/BreatheWater.java
@@ -0,0 +1,21 @@
+package net.adwr.breathewater;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BreatheWater implements ModInitializer {
+
+    public static final String MOD_ID = "breathe_water";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/adwr/breathewater/BreatheWaterClient.java
@@ -0,0 +1,10 @@
+package net.adwr.breathewater;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BreatheWaterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
